8d9e1df7f6774287313d6251fdd1fcbb0129162b
roman11x <romanmax1000@gmail.com>
2026-02-25 13:37:13 +0200

    Implemented first batch of databas functions

--- db.py
+++ db.py
@@ -1,13 +1,14 @@
 # Database class; all SQL; nothing else touches sqlite3
 import sqlite3
 
-class database:
+class Database:
     def __init__(self, path):
         self.path = path
-
+        self.con = sqlite3.connect(path)
+        self.con.row_factory = sqlite3.Row
     def setup(self):
-        with sqlite3.connect(self.path) as con:
-            con.executescript("""
+        with self.con as conn:
+            conn.executescript("""
             CREATE TABLE IF NOT EXISTS media(
             mal_id INTEGER NOT NULL,
             media_type TEXT NOT NULL,
@@ -50,5 +51,34 @@ class database:
                     );
                 CREATE TABLE IF NOT EXISTS settings(
                 key TEXT PRIMARY KEY,
-                value TEXT NOT NULL)
+                value TEXT NOT NULL);
             """)
+    def add_media(self,mal_id,media_type,title,status, tags, liked=None):
+        cursor = self.con.cursor()
+        cursor.execute('''
+        INSERT INTO media(mal_id,media_type,title,status,liked) values (?,?,?,?,? )''', (mal_id,media_type,title,status,liked))
+
+        for tag in tags:
+            cursor.execute('''
+            INSERT OR IGNORE INTO tags(mal_id,media_type,tag) values (?,?,?)''', (mal_id,media_type,tag))
+        self.con.commit()
+
+    def get_all_media(self, media_type = None) -> list[dict] :
+        query = "SELECT * FROM media WHERE status != 'blacklisted'"
+        params = []
+        if media_type is not None:
+            query += " AND media_type = ?"
+            params.append(media_type)
+        cursor = self.con.cursor()
+        cursor.execute(query, params)
+        return [dict(row) for row in cursor.fetchall()]
+
+
+    def get_media(self, mal_id, media_type) -> dict | None:
+        cursor = self.con.cursor()
+        cursor.execute('''
+        Select * from media WHERE mal_id =? and media_type = ?''', (mal_id, media_type))
+        row = cursor.fetchone()
+        if row is None:
+            return None
+        return dict(row)
--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-import db
+from db import Database
 
-database = db.database("data/media.db")
-database.setup()
+db = Database("data/media.db")
+db.setup()
+
+db.add_media(11, "manga", "Naruto", "consumed", ["Action", "Adventure", "Fantasy"], liked=1)
+db.add_media(1, "manga", "Berserk", "consuming", ["Action", "Dark Fantasy", "Seinen"])
+
+print(db.get_all_media())
+print(db.get_all_media("manga"))
+print(db.get_media(11, "manga"))
+print(db.get_media(99, "manga"))  # should return None
